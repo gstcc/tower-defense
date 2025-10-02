6aa9e09f89058f561f281e3feab6be94e4df8b76
thean981 <thean981@student.liu.se>
2025-10-02 15:26:51 +0200

    added end scenes for failed/successful levels

--- castle_defenders/StartMenu.cs
+++ castle_defenders/StartMenu.cs
@@ -4,7 +4,10 @@ using System;
 public partial class StartMenu : Node2D
 {
 	public void _OnButtonPressed() {
-		GD.Print("button pressed");
 		GetTree().ChangeSceneToFile("res://main.tscn");
+	}
+	
+	public void _OnButtonQuitPressed() {
+		GetTree().Quit();
 	}
 }
